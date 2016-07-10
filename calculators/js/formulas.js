2947c184b1bc9273d2223fe2ef4d204eeea6c36b
Zach Roetzel <zach@telmate.com>
2016-07-10 16:38:07 -0700

    adding celcius to farenheit fuction, fixing arguements for hypotenuse calculation

--- calculators/js/formulas.js
+++ calculators/js/formulas.js
@@ -6,15 +6,22 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
-// Faranheit <> Celcius conversion
+// Faranheit > Celcius conversion
 function calcFahrenheitToCelcius(f) {
 	return (f - 32) * (5/9)
 }
+// Celcius > Faranheit
+function calcCelciusToFarenheit(c) {
+	return (c * (9/5)) + 32
+}
 // Circumference from radias conversion 
-function calcCircumference(c) {
-	return (2 * Math.PI) * c 
+function calcCircumference(circ) {
+	return (2 * Math.PI) * circ
 }
 // Hypotenuse calculator, using sides A and B 
-function calcLongestSide(h) {
-	return Math.sqrt((pyA.value * pyA.value) + (pyB.value * pyB.value))
+// function calcLongestSide(h) {
+// 	return Math.sqrt((pyA.value * pyA.value) + (pyB.value * pyB.value))
+// }
+function calcLongestSide(pyAVal, pyBVal) {
+	return Math.sqrt((pyAVal * pyAVal) + (pyBVal * pyBVal))	
 }

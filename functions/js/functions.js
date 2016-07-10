32e7376e88d28becb1fa607b1fadae0d57b70455
Zach Roetzel <zach@telmate.com>
2016-07-10 15:49:55 -0700

    adding changes for Week 2 homework

---
+++ functions/js/functions.js
@@ -0,0 +1,22 @@
+console.log("template loaded");
+console.log("functions")
+
+function multiply(x, y) {
+	var product = x * y;
+	return product;
+}
+
+function subtract(x, y) {
+	var difference = x - y;
+	return difference;
+}
+
+function divide(x, y) {
+	var quotient = x / y;
+	return quotient;
+}
+
+function add(x, y) {
+	var sum = x + y;
+	return sum;
+}
